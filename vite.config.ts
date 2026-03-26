5c8ac9247f422e8c6672958d8b7e7682807c60db
Dominik Richter <dominik.richter@gmail.com>
2026-03-26 09:22:47 -0700

    🧹 rename rsac26 + github actions for workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
+  base: '/rsac26/',
   publicDir: 'src/static',
   server: {
     port: 8182,
